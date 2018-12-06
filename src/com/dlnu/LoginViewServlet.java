acf5c8c476222fa9356d94719fcebca18518d04e
wtfwsbg <1599177445@qq.com>
2018-12-06 08:43:32 +0800

    2

--- src/com/dlnu/LoginViewServlet.java
+++ src/com/dlnu/LoginViewServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet(name = "LoginViewServlet",urlPatterns = "/loginView")
-public class LoginViewServlet extends HttpServlet {
+public class  LoginViewServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
---
+++ src/edu/dlnu/student.java
@@ -0,0 +1,40 @@
+package edu.dlnu;
+
+public class student {
+    private int id;
+    private  String name;
+    private  String address;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
